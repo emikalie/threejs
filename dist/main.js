a3e0080456f6bf98c9ebd2deefc765c18201274b
emikalie <euk304@psu.edu>
2025-10-17 13:41:20 -0400

    fixed js project

---
+++ dist/main.js
@@ -0,0 +1,58 @@
+// import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
+
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+// Create a cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+const cube = new THREE.Mesh(geometry, material);
+cube.position.z = -15;
+cube.position.x = 20;
+cube.rotation.x = -15;
+cube.rotation.y = -.8;
+
+//Lights
+const ambientLight = new THREE.AmbientLight(0xffffff);
+ambientLight.position.set(25, -15, -400);
+
+// Add controls
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = 3;
+
+
+scene.add(cube);
+
+// Animation loop
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    controls.update();
+    renderer.render(scene, camera);
+}
+animate();
+
+// Handle window resize
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
